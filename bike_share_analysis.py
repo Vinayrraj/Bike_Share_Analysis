62a9da38eb912d17a86ec6bb27f7a655400d28f9
Vinayraj Singh <Singh@Singhs-iMac.local>
2017-12-15 19:20:27 +0530

    Data Ordered

--- bike_share_analysis.py
+++ bike_share_analysis.py
@@ -124,3 +124,95 @@ for city in tests:
     #print(time_of_trip(example_trips[city], city))
     #print(tests[city])
     assert time_of_trip(example_trips[city], city) == tests[city]
+
+print("-"*30)
+
+def type_of_user(datum, city):
+    """
+    Takes as input a dictionary containing info about a single trip (datum) and
+    its origin city (city) and returns the type of system user that made the
+    trip.
+
+    Remember that Washington has different category names compared to Chicago
+    and NYC.
+    """
+
+    # YOUR CODE HERE
+    user_type = None
+    if city == "NYC" or city == "Chicago":
+        user_type = datum['usertype']
+    elif city == "Washington":
+        user_type = datum['Member Type']
+        if user_type == "Registered":
+            user_type = "Subscriber"
+        else:
+             user_type = "Customer"
+
+    return user_type
+
+
+# Some tests to check that your code works. There should be no output if all of
+# the assertions pass. The `example_trips` dictionary was obtained from when
+# you printed the first trip from each of the original data files.
+tests = {'NYC': 'Customer',
+         'Chicago': 'Subscriber',
+         'Washington': 'Subscriber'}
+
+for city in tests:
+    assert type_of_user(example_trips[city], city) == tests[city]
+
+
+print("-"*30)
+
+def condense_data(in_file, out_file, city):
+    """
+    This function takes full data from the specified input file
+    and writes the condensed data to a specified output file. The city
+    argument determines how the input file will be parsed.
+
+    HINT: See the cell below to see how the arguments are structured!
+    """
+
+    with open(out_file, 'w') as f_out, open(in_file, 'r') as f_in:
+        # set up csv DictWriter object - writer requires column names for the
+        # first row as the "fieldnames" argument
+        out_colnames = ['duration', 'month', 'hour', 'day_of_week', 'user_type']
+        trip_writer = csv.DictWriter(f_out, fieldnames = out_colnames)
+        trip_writer.writeheader()
+
+        ## TODO: set up csv DictReader object ##
+        trip_reader = csv.DictReader(f_in)
+
+        # collect data from and process each row
+        for row in trip_reader:
+            # set up a dictionary to hold the values for the cleaned and trimmed
+            # data point
+            new_point = {}
+
+            ## TODO: use the helper functions to get the cleaned data from  ##
+            ## the original data dictionaries.                              ##
+            ## Note that the keys for the new_point dictionary should match ##
+            ## the column names set in the DictWriter object above.         ##
+            new_point['duration'] = duration_in_mins(row, city)
+            new_point['month'], new_point['hour'], new_point['day_of_week'] = time_of_trip(row, city)
+            new_point['user_type'] = type_of_user(row, city)
+
+
+            ## TODO: write the processed information to the output file.     ##
+            ## see https://docs.python.org/3/library/csv.html#writer-objects ##
+            trip_writer.writerow(new_point)
+
+
+print("-"*30)
+
+# Run this cell to check your work
+city_info = {'Washington': {'in_file': '/Users/Singh/Documents/PythonWorkspace/Machine Learning/Bike_Share_Analysis/data/Washington-CapitalBikeshare-2016.csv',
+                            'out_file': '/Users/Singh/Documents/PythonWorkspace/Machine Learning/Bike_Share_Analysis/data/Washington-2016-Summary.csv'},
+             'Chicago': {'in_file': '/Users/Singh/Documents/PythonWorkspace/Machine Learning/Bike_Share_Analysis/data/Chicago-Divvy-2016.csv',
+                         'out_file': '/Users/Singh/Documents/PythonWorkspace/Machine Learning/Bike_Share_Analysis/data/Chicago-2016-Summary.csv'},
+             'NYC': {'in_file': '/Users/Singh/Documents/PythonWorkspace/Machine Learning/Bike_Share_Analysis/data/NYC-CitiBike-2016.csv',
+                     'out_file': '/Users/Singh/Documents/PythonWorkspace/Machine Learning/Bike_Share_Analysis/data/NYC-2016-Summary.csv'}}
+
+for city, filenames in city_info.items():
+    condense_data(filenames['in_file'], filenames['out_file'], city)
+    print_first_point(filenames['out_file'])
